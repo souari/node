1570051426b5b12a6a8abd04ebb9ccfe5eed82a5
salim.ouari <salim.ouari@gmail.com>
2017-03-03 23:49:22 +0100

    add method

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var app = require('express')();
 
-app.get('/', function(){
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/add', function (req, res) {
+  res.send(req.query.a + req.query.b);
 });
 
