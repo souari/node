c433a54692a785ff102c69d14dd505502c474f8c
salim.ouari <salim.ouari@gmail.com>
2017-03-04 18:47:39 +0100

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 var app = require('express')();
 
+/**
+ * 
+ */
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    res.send('Welcome to node calculator');
 });
 
 app.get('/add', function (req, res) {
-  res.send(req.query.a + req.query.b);
+	try {
+		res.send((parseInt(req.query.a) + parseInt(req.query.b)).toString());	
+	} catch (error) {
+		res.send(error);
+	}
 });
 
+app.get('/sub', function (req, res) {
+    try {
+		res.send((parseInt(req.query.a) - parseInt(req.query.b)).toString());	
+	} catch (error) {
+		res.send(error);
+	}
+});
+
+app.get('/mul', function (req, res) {
+    try {
+		res.send((parseInt(req.query.a) * parseInt(req.query.b)).toString());	
+	} catch (error) {
+		res.send(error);
+	}
+});
+
+app.get('/div', function (req, res) {
+    try {
+		res.send((parseInt(req.query.a) / parseInt(req.query.b)).toString());	
+	} catch (error) {
+		res.send(error);
+	}
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
